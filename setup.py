553c304d16f41045180b1607001eef11c60e5b15
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-07-20 19:55:08 +0300

    SXDEDPCXZIC-198 / update check-link extension

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     namespace_packages=['ckanext'],
 
     install_requires=[
-        "ckanext-check-link~=0.0.16",
+        "ckanext-check-link~=0.1.2",
         "ckanext-oidc-pkce~=0.2.3",
       # CKAN extensions should not list dependencies here, but in a separate
       # ``requirements.txt`` file.
