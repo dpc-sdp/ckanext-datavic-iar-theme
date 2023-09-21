f8ae76d089485801735efe34c651e0c1812684f5
amorev <aleksey.morev@linkdigital.com.au>
2023-09-22 01:24:31 +0300

    SXDEDPCXZIC-230 / fix search suggestions styles

--- ckanext/datavic_iar_theme/plugin.py
+++ ckanext/datavic_iar_theme/plugin.py
@@ -3,19 +3,21 @@ import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 import ckanext.datavic_iar_theme.helpers as helpers
 
+from ckanext.search_autocomplete.interfaces import ISearchAutocomplete
+
 log = logging.getLogger(__name__)
 
 class DatavicIARThemePlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer)
     p.implements(p.ITemplateHelpers)
     p.implements(p.IMiddleware, inherit=True)
+    p.implements(ISearchAutocomplete)
 
     def make_middleware(self, app, config):
         app.before_request(lambda: log.info("Session cookie: %s", tk.request.headers.get("cookie")))
         return app
 
     # IConfigurer
-
     def update_config(self, config_):
         tk.add_template_directory(config_, "templates")
         tk.add_public_directory(config_, "public")
@@ -30,6 +32,13 @@ class DatavicIARThemePlugin(p.SingletonPlugin):
         )
 
     # ITemplateHelpers
-
     def get_helpers(self):
         return helpers.get_helpers()
+
+    # ISearchAutocomplete
+    def get_categories(self):
+        return {
+            'organization': tk._('Organisations'),
+            'res_format': tk._('Formats'),
+            'groups': tk._('Categories'),
+        }
