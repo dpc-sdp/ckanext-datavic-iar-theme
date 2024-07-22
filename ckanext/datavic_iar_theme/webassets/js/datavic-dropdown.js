d04b946463eecaacfd5c3cf4422ec8b4c9e6fc00
mutantsan <kvaqich@gmail.com>
2024-07-22 10:28:11 +0300

    SXDEDPCXZIC-345_DATAVIC-732 / update for mobile menu, show menu for non-logged users

--- ckanext/datavic_iar_theme/webassets/js/datavic-dropdown.js
+++ ckanext/datavic_iar_theme/webassets/js/datavic-dropdown.js
@@ -14,20 +14,16 @@ this.ckan.module("datavic-dropdown", function ($) {
                 if ($(window).width() <= 991) {
                     header = $("#mobile-menu").children(".rpl-site-header__inner");
                     dropdown_top = $(this).offset().top
-                    $(this).next(".dropdown").css("transform", `translate(40px, ${dropdown_top}px)`);
                 } else {
                     header = $("#main-menu").children(".rpl-site-header__inner");
                     dropdown_top = header.offset().top + header.outerHeight(true) - 2
                     var dropdown_height = $("body").outerHeight() - dropdown_top
-                    $(this).next(".dropdown").css("transform", `translate(40px, ${dropdown_top}px)`);
                     $(this).next(".dropdown").css("height", `${dropdown_height}px`);
                 }
                 $(this).next(".dropdown").slideToggle();
                 $(this).toggleClass("show")
                 $(this).next(".dropdown").toggleClass("dropdown-shown");
                 header.toggleClass("dropdown-hidden").toggleClass("dropdown-shown");
-
-
             });
         },
     };
