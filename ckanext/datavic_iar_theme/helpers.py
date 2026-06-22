481351f2ce2958e022b3338ac4eff8a6e4124c05
Awang Setyawan <awang.setyawan@salsa.digital>
2026-06-22 18:52:55 +0700

    [DATAVIC-949] refactored the validator to use ckan methods

--- ckanext/datavic_iar_theme/helpers.py
+++ ckanext/datavic_iar_theme/helpers.py
@@ -20,10 +20,6 @@ from ckanext.scheming.helpers import scheming_get_dataset_schema
 import ckanext.datavic_iar_theme.config as conf
 import ckanext.datavicmain.const as const
 
-from urllib.parse import urlparse
-from ckan.logic.validators import email_validator as ckan_email_validator
-from ckan.lib.navl.dictization_functions import Invalid
-
 log = logging.getLogger(__name__)
 helper, get_helpers = Collector("vic_iar").split()
 
@@ -527,27 +523,12 @@ def datavic_user_image(user_id: str, size: int = 100) -> Union[Markup, str]:
 
 
 @helper
-def is_url(value) -> bool:
-    if not value:
-        return False
-
-    try:
-        parsed = urlparse(value)
-        if parsed.scheme and parsed.netloc and parsed.scheme in ("http", "https"):
-            return True
-    except (ValueError, TypeError):
-        pass
-
-    return False
-
-
-@helper
 def is_email(value) -> bool:
     if not value:
         return False
 
     try:
-        ckan_email_validator(value, {})
+        tk.get_validator("email_validator")(value, {})
         return True
-    except Invalid:
+    except tk.Invalid:
         return False
